cd15ccb40b2f13012cc36fd074e21e014c908232
WellSCosta <wellscostaofc@gmail.com>
2024-11-26 09:25:56 -0300

    Customização Swagger OpenAPI - Produto Service

--- ProdutoService/src/main/java/com/wellscosta/vendas/onlineconfig/OpenAPIConfig.java
+++ ProdutoService/src/main/java/com/wellscosta/vendas/onlineconfig/OpenAPIConfig.java
@@ -1,13 +1,13 @@
 package com.wellscosta.vendas.onlineconfig;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
@@ -16,11 +16,11 @@ public class OpenAPIConfig {
     public OpenAPI customOpenAPI(@Value("${application-version}") String appVersion) {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Serviço de vendas")
+                        .title("Serviço de produtos")
                         .version(appVersion)
-                        .description("Serviço para gerenciamento de vendas")
+                        .description("Serviço para gerenciamento de produtos")
                         .termsOfService("http://swagger.io/terms/")
-                        .license(new License().name("Apache 3.0").url("http://springdoc.org"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
                         .contact(new Contact().name("Wellington Santos").email("wellscostaofc@gmail.com")));
     }
 }
